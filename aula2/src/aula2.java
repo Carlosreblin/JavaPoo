560474d0ec0313a282b1017531e388238ac7d320
Carlos Eduardo Reblin <carlosereblin@gmail.com>
2026-01-26 16:05:19 -0300

    add aula3

--- aula2/src/aula2.java
+++ aula2/src/aula2.java
@@ -1,14 +1,14 @@
-public class aula2 {
+public class Aula2 {
     public static void main(String[] args) {
-        caneta c1 = new caneta();
+        Caneta c1 = new Caneta();
         c1.cor = "azul";
-        c1.ponta = 0.5f;
-        c1.tampada = false;
+        //c1.ponta = 0.5f;
+        //c1.tampada = false;
         c1.tampar();
         //c1.status();
         c1.rabiscar();
 
-        caneta c2 = new caneta();
+        Caneta c2 = new Caneta();
         c2.modelo = "Hostnet";
         c2.cor = "preta";
         c2.destampar();
--- aula3/src/Caneta.java
+++ aula3/src/Caneta.java
@@ -1,10 +1,10 @@
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
-    void status(){
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    public void status(){
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta "+ this.cor);
         System.out.println("ponta: " + this.ponta);
@@ -12,7 +12,7 @@ public class Caneta {
         System.out.println("está tampada? " +  this.tampada);
     }
 
-    void rabiscar (){
+    public void rabiscar (){
         if (!this.tampada) {
             System.out.println("Estou rabiscando");
         }else{
@@ -20,11 +20,11 @@ public class Caneta {
         }
     }
 
-    void tampar(){
+    public void tampar(){
         this.tampada = true;
     }
 
-    void destampar(){
+    public void destampar(){
         this.tampada = false;
     }
 
